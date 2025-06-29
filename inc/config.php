5ba7d8acdd335d93637b41896c53bf8fc1c0b1a1
rostov114 <Rostov114@gmail.com>
2025-06-30 00:47:38 +0400

    Add config

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php defined('DOCROOT') or die('No direct access allowed.');
+
+$_CONFIG = 
+[
+	'bucket' => '',
+	'region' => '',
+	'endpoint' => '',
+	'accessKey' => '',
+	'secretKey' => '',
+	'prefix' => '',
+	'custom_domain' => FALSE
+];
